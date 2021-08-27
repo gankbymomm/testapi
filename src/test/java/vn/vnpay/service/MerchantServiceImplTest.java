271eab6d8cfed18b4eacccd0066ebeb7a819c5d9
gankbymomm <53865276+gankbymomm@users.noreply.github.com>
2021-08-27 11:38:38 +0700

    Update MerchantServiceImplTest.java

--- src/test/java/vn/vnpay/service/MerchantServiceImplTest.java
+++ src/test/java/vn/vnpay/service/MerchantServiceImplTest.java
@@ -11,27 +11,15 @@ public class MerchantServiceImplTest {
 
     @Test
     public void getDataTest(){
-        ReqFromMerchantDTO reqFromMerchantDTO = new ReqFromMerchantDTO();
-        reqFromMerchantDTO.setAppId("VNPAY");
-        reqFromMerchantDTO.setServiceCode("03");
-        reqFromMerchantDTO.setMasterMerCode("A000000775");
-        reqFromMerchantDTO.setMerchantCode("036093001475");
-        reqFromMerchantDTO.setCcy("704");
-        reqFromMerchantDTO.setAmount("55000.00");
-        reqFromMerchantDTO.setCountryCode("CN");
-        reqFromMerchantDTO.setMerchantName("BEST TRANSPORT");
-        reqFromMerchantDTO.setMerchantCity("BEIJING");
-        reqFromMerchantDTO.setPostalCode("10000");
-        reqFromMerchantDTO.setBillNumber("987654");
-        reqFromMerchantDTO.setStoreLabel("1234");
-        reqFromMerchantDTO.setTerminalId("A6008667");
-        reqFromMerchantDTO.setPurpose("COSO2");
-        reqFromMerchantDTO.setAddInfo("ME");
-        reqFromMerchantDTO.setPayType("01");
-        reqFromMerchantDTO.setTxnId("123113277");
-        reqFromMerchantDTO.setExpDate("2107170755");
-        reqFromMerchantDTO.setChecksum("3C927D42F84B5420502AAF29B97402C5");
-        reqFromMerchantDTO.setCrc("8TTM");
+        String dataInput = "{\"appId\":\"VNPAY\",\"serviceCode\":\"03\",\"masterMerCode\":\"A000000775\"," +
+                "\"merchantCode\":\"036093001475\",\"ccy\":\"704\",\"amount\":\"55000.00\"," +
+                "\"countryCode\":\"CN\",\"merchantName\":\"BEST TRANSPORT\",\"merchantCity\":\"BEIJING\"," +
+                "\"postalCode\":\"10000\",\"billNumber\":\"987654\",\"storeLabel\":\"1234\",\"terminalId\":\"A6008667\"," +
+                "\"purpose\":\"COSO2\",\"addInfo\":\"ME\",\"payType\":\"01\",\"txnId\":\"123113277\",\"expDate\":\"2107170755\"," +
+                "\"checksum\":\"3C927D42F84B5420502AAF29B97402C5\",\"crc\":\"8TTM\"}";
+
+        ObjectMapper mapperToReqFromMerchant = new ObjectMapper();
+        ReqFromMerchantDTO reqFromMerchantDTO = mapperToReqFromMerchant.readValue(dataInput, ReqFromMerchantDTO.class);
 
         MerchantServiceImpl merchantService = new MerchantServiceImpl();
         String checksum = CheckSum.getCheckSum(reqFromMerchantDTO);
